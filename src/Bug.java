4977e24fbde743e7a88f0039fe00f6960bb28edd
Felix Holmertz <felix.holmertz@gmail.com>
2019-04-04 16:50:40 +0200

    Fixed bug class and fixed Hello World typo...MACKAN!!!

---
+++ src/Bug.java
@@ -0,0 +1,68 @@
+public class Bug{
+    private int id;
+    private String title;
+    private String description;
+    private String stepsToReproduce;
+    private enum priority{
+       HIGH,
+       MEDIUM,
+       LOW
+    };
+    private enum priority{
+        CRITICAL,
+        MAJOR,
+        MINOR
+    };
+    private enum status{
+        ACTIVE,
+        FIXED
+    };
+    private String comment;
+
+    public Bug(String title, String description, String stepsToReproduce, String comment) {
+        this.title = title;
+        this.description = description;
+        this.stepsToReproduce = stepsToReproduce;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStepsToReproduce() {
+        return stepsToReproduce;
+    }
+
+    public void setStepsToReproduce(String stepsToReproduce) {
+        this.stepsToReproduce = stepsToReproduce;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
---
+++ src/BugSplatter.java
@@ -0,0 +1,6 @@
+public class BugSplatter {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+
+}
